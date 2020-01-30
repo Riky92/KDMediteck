0ceed5ed1984b48fcdb38d6a0b624a9af3954b65
Riccardo Mencucci <riccardo.mencucci@prometeia.com>
2020-01-30 15:41:58 +0100

    update restyle css

--- src/app/dashboard/model/common-constants.ts
+++ src/app/dashboard/model/common-constants.ts
@@ -54,45 +54,46 @@ export const UsersKidney = [
       // { key: 'instagram', descrizione: 'Instagram', link: 'https://www.instagram.com/richimencu/?hl=it'},
       // { key: 'twitter', descrizione: 'Twitter', link: 'http://google.it'}
     ],
-    situazione_attuale: 'Ho terminato i miei studi presso l’ateneo di Ancona laureandomi in Medicina. Terminati gli studi ho scelto la specializzazione in Ortopedia, entrando all\'Istituto Ortopedico Rizzoli. Attualmente sono al secondo anno di specializzazione. Mi sono laureato nel 2017 presso l\' Università Politecnica delle Marche. Attualmente lavoro come specializzando in Ortopedia e Traumatologia (secondo Anno), presso l\' Istituto Ortopedico Rizzoli di Bologna.',
+    situazione_attuale: 'Mi sono laureato nel 2017 presso l\' Università Politecnica delle Marche. Attualmente lavoro come specializzando in Ortopedia e Traumatologia (secondo Anno), presso l\' Istituto Ortopedico Rizzoli di Bologna.',
     working_experencies: [
-      {id: 'Istituto Ortopedico Rizzoli', timeWork: '[01/11/2018 - Oggi]', role: 'Medico Chirurgo, specializzando in Ortopedia e Traumatologia', position: 'II anno di specializzazione', main_activities: 'Attività Diagnostiche e Chirurgiche di patologie ortopediche a carattere oncologico'}
+      {id: 'Istituto Ortopedico Rizzoli', timeWork: '[01/11/2018 - Oggi]', role: 'Medico Chirurgo, specializzando in Ortopedia e Traumatologia', position: 'II anno di specializzazione', main_activities: 'Attività Diagnostiche e Chirurgiche di patologie ortopediche prevalentemente a carattere oncologico e sportivo'}
     ],
     formazione: [
-        {id: 'Università Politecnica delle Marche Facoltà di Medicina e Chirurgia', tipo: 'Laurea Magistrale in Medicina e Chirurgia', luogo: 'Ancona (AN)', voto: '110 e lode/110', data: '25/07/2017', tesi: 'Ruolo prognostico e predittivo della dimensione delle metastasi epatiche nei pazienti affetti da calcinoma del colon-retto trattati con anti-VEGF e anti-EGFR'},
-        {id: 'Liceo Scientifico G.Torelli', tipo: 'Scuola superiore', luogo: 'Fano(PU)', voto: '100/100 e lode', data: '11/07/2011', tesi: 'Il ricercatore con modello Superuomo'}
+        {id: 'Università Politecnica delle Marche Facoltà di Medicina e Chirurgia', tipo: 'Laurea Magistrale in Medicina e Chirurgia', luogo: 'Ancona (AN)', voto: '110 e lode/110', data: '25/07/2017', tesi: 'Ruolo prognostico e predittivo della dimensione delle metastasi epatiche nei pazienti affetti da carcinoma del colon-retto trattati con anti-VEGF e anti-EGFR'},
+        {id: 'Liceo Scientifico G.Torelli', tipo: 'Scuola superiore', luogo: 'Fano(PU)', voto: '100/100 e lode', data: '11/07/2011', tesi: 'Il ricercatore come moderno Superuomo'}
     ],
     skills: 'Team building, ricerca'
-  },
-  {
-    id: 'ciavaglia_camilla',
-    nome: 'Ciavaglia Camilla',
-    foto: 'cami',
-    citta_natale: 'Fano',
-    citta_attuale: 'Bologna',
-    area: 'Responsabile legale',
-    birthday: '28/06/1993',
-    degree: 'Dott. in Giurisprudenza',
-    mansione: 'Praticante Avvocato',
-    email: 'camilla.ciavaglia28@gmail.com',
-    lenguages: [
-      {key: 'it', foto: 'italy'},
-      {key: 'eng', foto: 'england'}
-    ],
-    canaliDiContatto: [
-      { key: 'linkedin', descrizione: 'Linkedin', link: 'https://www.linkedin.com/in/camilla-ciavaglia-51381415b/'},
-      // { key: 'facebook', descrizione: 'Facebook', link:'http://google.it'},
-      { key: 'instagram', descrizione: 'Instagram', link: 'https://www.instagram.com/camilla.ciavaglia/?hl=it'},
-      // { key: 'twitter', descrizione: 'Twitter', link: 'http://google.it'}
-    ],
-    situazione_attuale: 'Ho terminato i miei studi presso l’ateneo di Bologna laureandomi in Giurisprudenza. Laurea a ciclo unico conseguita in 5 anni. Nel periodo post-laurea ho iniziato il periodo di tirocinio forense presso lo studio legale boutique <<De Vergottini>> ( sede di Bologna), specializzato in  consulenza legale in Antitrust, diritto commerciale, contrattualistica',
-    working_experencies: [
-                        {id: 'Studio legale boutique <<De Vergottini>>', timeWork: '[01/04/2018 - Oggi]', role: 'Praticante avvocato', position: 'Tirocinio forense', main_activities: 'consulenza legale, ricerca redazione di atti giudiziari e pareri legali'},
-    ],
-    formazione: [
-        {id: 'Facoltà di Giurisprundenza - Università degli studi di Bologna', tipo: 'Laurea  Magistrale a Ciclo Unico', luogo: 'Bologna', voto: '105/110', data: '21/02/2019', tesi: 'Separazione e mobbing coniugale nelle ricerche giurisprudenziali del XVII secolo'},
-        {id: 'Liceo classico G.Nolfi', tipo: 'Scuola superiore' , luogo: 'Fano', voto: '84/100', data: '01/07/2011', tesi: 'Studio delle origini della lingua greca e latina'},
-    ],
-    skills: 'Redazione di atti e pareri legali, risoluzione di problematiche giuridiche complesse, abilità nell’utilizzo di strumenti informatici, in particolare banche dati giuridiche Leggi d’ Italia professionale e De jure'
-  }
+	}
+	// ,
+  // {
+  //   id: 'ciavaglia_camilla',
+  //   nome: 'Ciavaglia Camilla',
+  //   foto: 'cami',
+  //   citta_natale: 'Fano',
+  //   citta_attuale: 'Bologna',
+  //   area: 'Responsabile legale',
+  //   birthday: '28/06/1993',
+  //   degree: 'Dott. in Giurisprudenza',
+  //   mansione: 'Praticante Avvocato',
+  //   email: 'camilla.ciavaglia28@gmail.com',
+  //   lenguages: [
+  //     {key: 'it', foto: 'italy'},
+  //     {key: 'eng', foto: 'england'}
+  //   ],
+  //   canaliDiContatto: [
+  //     { key: 'linkedin', descrizione: 'Linkedin', link: 'https://www.linkedin.com/in/camilla-ciavaglia-51381415b/'},
+  //     // { key: 'facebook', descrizione: 'Facebook', link:'http://google.it'},
+  //     { key: 'instagram', descrizione: 'Instagram', link: 'https://www.instagram.com/camilla.ciavaglia/?hl=it'},
+  //     // { key: 'twitter', descrizione: 'Twitter', link: 'http://google.it'}
+  //   ],
+  //   situazione_attuale: 'Ho terminato i miei studi presso l’ateneo di Bologna laureandomi in Giurisprudenza. Laurea a ciclo unico conseguita in 5 anni. Nel periodo post-laurea ho iniziato il periodo di tirocinio forense presso lo studio legale boutique <<De Vergottini>> ( sede di Bologna), specializzato in  consulenza legale in Antitrust, diritto commerciale, contrattualistica',
+  //   working_experencies: [
+  //                       {id: 'Studio legale boutique <<De Vergottini>>', timeWork: '[01/04/2018 - Oggi]', role: 'Praticante avvocato', position: 'Tirocinio forense', main_activities: 'consulenza legale, ricerca redazione di atti giudiziari e pareri legali'},
+  //   ],
+  //   formazione: [
+  //       {id: 'Facoltà di Giurisprundenza - Università degli studi di Bologna', tipo: 'Laurea  Magistrale a Ciclo Unico', luogo: 'Bologna', voto: '105/110', data: '21/02/2019', tesi: 'Separazione e mobbing coniugale nelle ricerche giurisprudenziali del XVII secolo'},
+  //       {id: 'Liceo classico G.Nolfi', tipo: 'Scuola superiore' , luogo: 'Fano', voto: '84/100', data: '01/07/2011', tesi: 'Studio delle origini della lingua greca e latina'},
+  //   ],
+  //   skills: 'Redazione di atti e pareri legali, risoluzione di problematiche giuridiche complesse, abilità nell’utilizzo di strumenti informatici, in particolare banche dati giuridiche Leggi d’ Italia professionale e De jure'
+  // }
 ];
